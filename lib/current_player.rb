582786955f178d4ae49d0d7dd81c801522c8df65
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-07 12:17:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,19 @@
 # Sets a counter for turn number
 def turn_count(board)
   i = 0
-  
-  board.each do |i|
-      if i == 'X' || i == 'O'
-        i += 1
-      end
-    end
 
-            return
+  board.each do |x|
+    if x == 'X' || x == 'O'
+      i += 1
+    end
+  end
+
+  return i
 end
 
-# 
-def current_player()
+# Sets Current Player, X/O
+def current_player(board)
+
+  turn = turn_count(board).even? ? "X" : "O"
 
 end
